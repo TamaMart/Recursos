a7cb707ec984562c04266d995c782dabce82b6c7
Tamara M. <t.mart.esc@gmail.com>
2026-04-18 18:35:05 -0400

    Add files via upload

---
+++ Python/Practica/Ejercicio 4.py
@@ -0,0 +1,12 @@
+#Listas
+
+personas = ["Juan", "María", "Pedro", "Ana"]
+print(personas)
+print(personas[0]) # Acceder al primer elemento
+print(personas[1]) # Acceder al segundo elemento
+print(personas[2]) # Acceder al tercer elemento
+print(personas[3]) # Acceder al cuarto elemento 
+
+for persona in personas:
+    print("- " + persona)
+    
